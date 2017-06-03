6a23565fb8e50c1558af7610d31a9c9a2b7c04b3
oguz <grafikcoder@gmail.com>
2017-06-04 00:25:42 +0300

    Upload

---
+++ dist/js/smart-mobile-menu.js
@@ -0,0 +1,87 @@
+/***
+ * @author Oguzhan Külcü - grafikcoder@gmail.com - oguzhankulcu.com
+ * @version 1.0.0
+ * http://oguzkulcu.github.io/smart-mobile-menu/
+ */
+(function ($) {
+    "use strict";
+
+    $.fn.smobileMenu = function (param) {
+        // Start Code
+
+        var thisContent = '__smobileMenuContent__menu';
+
+        var data = $.extend({
+            dropdown: true,
+            getMenu: null,
+            theme: 'default',
+            dropdownIcon: '<i class="fa fa-angle-down"></i>',
+            linkActiveClass: 'smm-active',
+            activeAutoOpen: true,
+            closeClickEvent: null, // function
+            openClickEvent: null // function
+        }, param);
+
+
+        $('body').prepend('<div id="' + thisContent + '"></div>');
+
+        var thisContentSelector = $('#' + thisContent),
+            getMenuContent = $(data.getMenu).html(),
+            theme_prefix = 'smobilemenu-theme-';
+
+
+        thisContentSelector.html('<a href="#" id="smobileMenu-close-btn">X</a><ul>' + getMenuContent + '</ul>');
+
+        thisContentSelector.find('ul,li').removeAttr('class');
+
+        var ul = thisContentSelector.children('ul');
+
+        ul.addClass(theme_prefix + data.theme);
+        ul.find('ul[data-smm-hidden=true],li[data-smm-hidden=true]').hide();
+        ul.find('ul').hide();
+
+        //    var countLi = ul.children('li').not('[data-smm-hidden=true]').length;
+
+        if (data.dropdown) {
+            ul.find('li a + ul').not('ul[data-smm-dropdown=false]').prev('a').append(data.dropdownIcon);
+
+            ul.find('li a + ul').not('ul[data-smm-dropdown=false]').prev('a').on('click', function (e) {
+                $(this).next('ul').toggle(200);
+                e.preventDefault();
+            });
+        }
+
+        if (data.activeAutoOpen) {
+            var active = ul.find('li a.' + data.linkActiveClass);
+            active.next('ul').show();
+            active.closest('ul').show();
+        }
+
+        // close button
+        $('#smobileMenu-close-btn').on('click', function (e) {
+            if (data.closeClickEvent == null) {
+                thisContentSelector.hide(200);
+            } else {
+                data.closeClickEvent($(thisContentSelector));
+            }
+            e.preventDefault();
+        });
+
+        // data-smm-hidden
+
+        $(this).on('click', function (e) {
+
+            // openClickEvent
+
+            if (data.openClickEvent == null) {
+                thisContentSelector.show(200);
+            } else {
+                data.openClickEvent($(thisContentSelector));
+            }
+            e.preventDefault();
+        });
+
+        // End Code
+    };
+
+})(jQuery);
